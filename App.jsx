5ab797dd481a75a067e894dbfddf2a1dbced0a78
Santosh-NPL <64217524+Santosh-NPL@users.noreply.github.com>
2024-12-25 20:20:49 +0545

    First App, View, Text, Button

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import { View, Text, Button } from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text style = { { fontSize : 30}}>Shreenisha Neupane</Text>
+      <Text>Bharatpur -12, Chitwan</Text>
+      <Text>Phone no. 9845563649</Text>
+      <Button title="Press Here" />
+
+    </View>
+  )
+}
+
+export default App
+
+//rnfe
